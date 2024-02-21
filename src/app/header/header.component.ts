e2cf01704f955f7523fc7b516a6357a18940f89e
Ashish Kumar <zashu001@gmail.com>
2024-02-21 11:27:03 +0530

    header components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MainMenuComponent, TopMenuComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
