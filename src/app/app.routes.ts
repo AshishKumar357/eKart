4b9e2fe8996b6ea39335dbbac2af2302d3ca469a
Ashish Kumar <zashu001@gmail.com>
2024-02-21 11:03:15 +0530

    footer and header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+//   { path: 'home', component: HomePageComponent },
+//   { path: 'about', component: AboutComponent },
+//   { path: 'contact', component: ContactComponent },
+//   { path: 'download', component: DownloadComponent },
+//   { path: 'catalog', component: CatalogComponent },
+//   { path: 'termsAndConditions', component: TermsAndConditionsComponent },
+//   { path: 'privacyPolicy', component: PrivacyPolicyComponent },
+];
