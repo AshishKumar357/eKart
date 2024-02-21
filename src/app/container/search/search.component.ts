498e7d13750394213cdffd12f14a8bb7942da918
Ashish Kumar <zashu001@gmail.com>
2024-02-21 15:56:43 +0530

    product list contents

--- src/app/container/search/search.component.ts
+++ src/app/container/search/search.component.ts
@@ -1,14 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
 export class SearchComponent {
+  searchClicked(_t3: HTMLInputElement) {
+    throw new Error('Method not implemented.');
+  }
   searchText: string = '';
 
   // ngOnInit()
